b118d5f17acaf7378e017cdec60c865adffd18ca
NicholasMiron <nicholasmiron@gmail.com>
2019-03-04 12:37:05 -0600

    Made everything awesome

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+var App = () => (
+    <div>
+        <GroceryList todos={['Eat a hippo', 'Be a hippo', 'Rule the Jungle']}/>
+    </div>
+);
+
+var GroceryList = (props) => {
+  return (
+    <ul>
+          {/* <GroceryListItem item='water buffalo'/> */}
+          {props.todos.map((todo) => { return <GroceryListItem key={todo} item={todo} />})}
+      </ul>
+    );
+  };
+  
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      done: false,
+      hovered: false,
+    }
+  }
+  onListItemClick(event) {
+    console.log("I love hippos!", this.state.done)
+    this.setState({done:!this.state.done});
+  };
+
+  onMouseHover(event) {
+    this.setState({hovered: true});
+  }
+  
+  onMouseExit(event) {
+    this.setState({hovered: false});
+  }
+  render() {
+    var style = { textDecoration: this.state.done ? 'line-through' : 'none', fontWeight: this.state.hovered ? 'bold' : 'normal'};
+    return (
+      <li 
+        style={style} 
+        onMouseOver={this.onMouseHover.bind(this)}
+        onMouseLeave={this.onMouseExit.bind(this)} 
+        onClick={this.onListItemClick.bind(this)}
+      
+      >{this.props.item}</li>
+    )
+  };
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
